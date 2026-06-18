adf0e42c1512529267ec9cdcee107cdac9405f01
soraya <soraya.albuquerque98@gmail.com>
2026-06-18 13:44:57 -0300

    ignorando uma pasta

--- nomes.py
+++ nomes.py
@@ -1,2 +1,3 @@
 nome= "laura";
+outroNome = "Milena";
 print(nome);
---
+++ outraPasta/Codigo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Codigo{
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Insira o filme que você gostaria de assistir: ");
+        nomeFilme = leitura.nextLine();
+        
+    }
+}
